c4842239779a9950be833e3ec915d712c8c3775d
AustinErickson <133445657+AustinErickson@users.noreply.github.com>
2023-12-30 02:48:57 -0800

    Delete scene.py

--- scene.py
+++
@@ -1,8 +0,0 @@
-# Scene is a container for all objects (except the camera)
-
-import numpy as np
-
-class Scene():
-    def __init__(self, masses = np.array([]), spheres = np.array([])):
-        self.masses = masses
-        self.spheres = spheres
